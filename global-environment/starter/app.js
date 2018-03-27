955c5fef66bd06c243024e0e5776a8c948d61d15
Sam Ojling <s.ojling@gmail.com>
2018-03-27 14:21:50 +0200

    Started the JS understanding the weird parts on Udemy

---
+++ global-environment/starter/app.js
@@ -0,0 +1,8 @@
+// variable get assigned to the global executed context
+// global executed context is !== inside functions
+// global object can be access through the window object or 'this' keyword
+var a = 'Hello world';
+
+function b() {
+
+}
